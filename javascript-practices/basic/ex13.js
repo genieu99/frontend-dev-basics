90d52bce379e561d953603512b73dbd9be99b3a1
genieu99 <yujin0419@sookmyung.ac.kr>
2024-07-04 13:16:46 +0900

    class: js basics #7

--- javascript-practices/basic/ex13.js
+++ javascript-practices/basic/ex13.js
@@ -2,3 +2,27 @@
 string primitive 타입과 String 객체 함수(String.prototype.*)
 */
 
+// casting
+var str5 = "5" + 5;
+console.log(str5);
+
+var str6 = "boolean:" + true;
+console.log(str6);
+
+// 객체 함수
+var str7 = "string1 string2 string3";
+
+var index = str7.indexOf('string2');  // 8
+console.log(index);
+
+index = str7.indexOf('string4');  // -1
+console.log(index);
+
+var str8 = str7.substring(10/*firstIndex*/, 13/*lastIndex - 1*/);
+console.log(str8);
+
+var a = str7.split(" ");
+console.log(a);
+
+a = str7.split(":");
+console.log(a);
